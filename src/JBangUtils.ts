e49707dba29df4c8e4ba71748badc724935edf79
Fred Bricon <fbricon@gmail.com>
2022-07-14 19:37:33 +0200

    Fix JBang script detection
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- src/JBangUtils.ts
+++ src/JBangUtils.ts
@@ -1,13 +1,13 @@
 export function isJBangFile(content: string | string[]): boolean {
     let lines:string[] = [];
-    if (content instanceof String) {
+    if (typeof content === 'string' || content instanceof String) {
         lines = content.split(/\r?\n/);
     } else {
         lines = content as string[]; 
     }
     for (let i = 0; i < lines.length; i++) {
         const line = lines[i];
-        if (line.startsWith("//DEPS" || line.startsWith("//JAVA") || line.startsWith("///usr/bin/env jbang"))) {
+        if (line.startsWith("//DEPS") || line.startsWith("//JAVA") || line.startsWith("///usr/bin/env jbang")) {
             return true;
         }
     }
