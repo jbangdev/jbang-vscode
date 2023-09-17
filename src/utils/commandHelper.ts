1b7e147faa249e3ab25a4e10e798000a6cf9c414
Fred Bricon <fbricon@gmail.com>
2023-09-17 14:40:21 +0200

    fix: ensure commands can run from command palette
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

---
+++ src/utils/commandHelper.ts
@@ -0,0 +1,24 @@
+import { Uri, window } from "vscode";
+import { isJBangFile } from "../JBangUtils";
+
+export async function handleCommand(callback: (uri: Uri) => Promise<any>, uri: any) {
+  const scriptUri = getValidScriptUri(uri);
+  if (scriptUri) {
+      return callback(scriptUri);
+  }
+  showErrorMessage();
+}
+
+export function getValidScriptUri(uri ?: Uri): Uri | undefined {
+  if (uri) {
+      return uri;
+  }
+  const activeEditor = window.activeTextEditor;
+  if (activeEditor && activeEditor.document && isJBangFile(activeEditor.document.getText())) {
+      return activeEditor.document.uri;
+  }
+}
+
+export function showErrorMessage() {
+  window.showErrorMessage("You must select a JBang script");
+}
